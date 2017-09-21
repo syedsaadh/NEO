f2eef6f3ca937ed78a01cbf8622628ef14958f5a
Syed Saad <hassan.saad.mail@gmail.com>
2017-09-21 21:45:42 +0530

    NEO Gson Deserializer added for Date Wise Objects

---
+++ app/src/main/java/com/xeda/projectmeteor/utils/NeoDeserializer.java
@@ -0,0 +1,61 @@
+package com.xeda.projectmeteor.utils;
+
+import com.google.gson.Gson;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonDeserializationContext;
+import com.google.gson.JsonDeserializer;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParseException;
+import com.google.gson.reflect.TypeToken;
+import com.xeda.projectmeteor.models.Links;
+import com.xeda.projectmeteor.models.MeteorObjects;
+import com.xeda.projectmeteor.models.NEO;
+import com.xeda.projectmeteor.models.NearEarthObjects;
+
+import org.json.JSONObject;
+
+import java.lang.reflect.Type;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Created by Xeda on 21-09-2017.
+ */
+
+public class NeoDeserializer implements JsonDeserializer<NEO> {
+    @Override
+    public NEO deserialize(JsonElement json, Type typeOfT, JsonDeserializationContext context) throws JsonParseException {
+        NEO n = new NEO();
+        final JsonObject jsonObject = json.getAsJsonObject();
+
+        Integer elementCount = jsonObject.get("element_count").getAsInt();
+
+        Links links = new Links();
+        links.setNext(jsonObject.get("next").getAsString());
+        links.setNext(jsonObject.get("prev").getAsString());
+        links.setNext(jsonObject.get("self").getAsString());
+
+        JsonObject nearEarthObjects = jsonObject.getAsJsonObject("near_earth_objects");
+
+        List<NearEarthObjects> list = new ArrayList<NearEarthObjects>();
+
+        for (Map.Entry<String, JsonElement> entry : nearEarthObjects.entrySet()){
+            NearEarthObjects nO = new NearEarthObjects();
+            String key = entry.getKey();
+            JsonElement nearEarthObjectsJson = entry.getValue();
+            List<MeteorObjects> mList = new ArrayList<MeteorObjects>();
+            mList = new Gson().fromJson(nearEarthObjectsJson, new TypeToken<List<MeteorObjects>>(){}.getType());
+            nO.setDate(key);
+            nO.setMeteorObjects(mList);
+            list.add(nO);
+        }
+
+        n.setElementCount(elementCount);
+        n.setLinks(links);
+        n.setNearEarthObjects(list);
+        return n;
+    }
+}
