2e08810fb53a6ae826fd009564a605383d01c2e5
Syed Saad <hassan.saad.mail@gmail.com>
2017-09-24 14:45:49 +0530

    Updates to Main Activity

--- app/src/main/java/com/xeda/projectmeteor/MainActivity.java
+++ app/src/main/java/com/xeda/projectmeteor/MainActivity.java
@@ -4,12 +4,40 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.MenuItem;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.xeda.projectmeteor.api.ApiClient;
+import com.xeda.projectmeteor.api.NEOService;
+import com.xeda.projectmeteor.models.NEO;
+import com.xeda.projectmeteor.models.NearEarthObjects;
+import com.xeda.projectmeteor.utils.DateConverter;
+
+import org.reactivestreams.Subscriber;
+import org.reactivestreams.Subscription;
+
+import java.util.Date;
+import java.util.List;
+
+import io.reactivex.Observable;
+import io.reactivex.Observer;
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.disposables.CompositeDisposable;
+import io.reactivex.disposables.Disposable;
+import io.reactivex.schedulers.Schedulers;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class MainActivity extends AppCompatActivity {
 
     private TextView mTextMessage;
+
+    private final static String API_KEY = BuildConfig.API_KEY;
+    private static final String TAG = MainActivity.class.getSimpleName();
+    private Observable<NEO> observable;
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -40,6 +68,44 @@ public class MainActivity extends AppCompatActivity {
         mTextMessage = (TextView) findViewById(R.id.message);
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+
+        if (API_KEY.isEmpty()) {
+            Toast.makeText(getApplicationContext(), "Please obtain your API KEY", Toast.LENGTH_LONG).show();
+            return;
+        }
+        NEOService neoService = ApiClient.getClient().create(NEOService.class);
+        observable = neoService.getFeeds("2015-09-07","2015-09-08",API_KEY);
+        observable
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(new Observer<NEO>() {
+                    @Override
+                    public void onSubscribe(@io.reactivex.annotations.NonNull Disposable d) {
+
+                    }
+
+                    @Override
+                    public void onNext(@io.reactivex.annotations.NonNull NEO neo) {
+                        Log.d(TAG, "Count of Near Earth Objects " + neo.getNearEarthObjects().get(0).getDate());
+                    }
+
+                    @Override
+                    public void onError(@io.reactivex.annotations.NonNull Throwable e) {
+
+                    }
+
+                    @Override
+                    public void onComplete() {
+
+                    }
+                });
+//                .subscribe( response -> {
+//                    Log.d(TAG, "Count of Near Earth Objects " + response.getNearEarthObjects().get(0).getDate());
+//                });
     }
 
+    @Override
+    protected void onStop() {
+        super.onStop();
+    }
 }
