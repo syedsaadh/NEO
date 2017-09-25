164f6488ff82b8104f9c15136b3ea6911542f670
Syed Saad <hassan.saad.mail@gmail.com>
2017-09-25 22:40:29 +0530

    ApiClient Update

--- app/src/main/java/com/xeda/projectmeteor/api/ApiClient.java
+++ app/src/main/java/com/xeda/projectmeteor/api/ApiClient.java
@@ -2,7 +2,6 @@ package com.xeda.projectmeteor.api;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
-import com.jakewharton.retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory;
 import com.xeda.projectmeteor.models.NEO;
 import com.xeda.projectmeteor.utils.NeoDeserializer;
 
@@ -22,7 +21,7 @@ public class ApiClient {
         if (retrofit==null) {
             retrofit = new Retrofit.Builder()
                     .baseUrl(BASE_URL)
-                    .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+//                    .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
                     .addConverterFactory(buildGsonConverter())
                     .build();
         }
