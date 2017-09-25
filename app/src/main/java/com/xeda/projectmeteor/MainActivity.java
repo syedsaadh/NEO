d3886b9c24e19acbc853b720a19e6f9160132d53
Syed Saad <hassan.saad.mail@gmail.com>
2017-09-25 22:46:51 +0530

    Architecture Components Set up and Implement

--- app/src/main/java/com/xeda/projectmeteor/MainActivity.java
+++ app/src/main/java/com/xeda/projectmeteor/MainActivity.java
@@ -1,9 +1,15 @@
 package com.xeda.projectmeteor;
 
+import android.arch.lifecycle.LifecycleActivity;
+import android.arch.lifecycle.Observer;
+import android.arch.lifecycle.ViewModelProviders;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.MenuItem;
 import android.widget.TextView;
@@ -11,33 +17,32 @@ import android.widget.Toast;
 
 import com.xeda.projectmeteor.api.ApiClient;
 import com.xeda.projectmeteor.api.NEOService;
+import com.xeda.projectmeteor.models.MeteorObjects;
 import com.xeda.projectmeteor.models.NEO;
-import com.xeda.projectmeteor.models.NearEarthObjects;
-import com.xeda.projectmeteor.utils.DateConverter;
+import com.xeda.projectmeteor.ui.NEOAdapter;
+import com.xeda.projectmeteor.viewmodel.LiveDataNeoViewModel;
 
-import org.reactivestreams.Subscriber;
-import org.reactivestreams.Subscription;
 
-import java.util.Date;
+import java.util.ArrayList;
 import java.util.List;
 
 import io.reactivex.Observable;
-import io.reactivex.Observer;
 import io.reactivex.android.schedulers.AndroidSchedulers;
-import io.reactivex.disposables.CompositeDisposable;
 import io.reactivex.disposables.Disposable;
 import io.reactivex.schedulers.Schedulers;
-import retrofit2.Call;
-import retrofit2.Callback;
-import retrofit2.Response;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity  {
 
-    private TextView mTextMessage;
+    private RecyclerView mRecyclerView;
+    private NEOAdapter mAdapter;
+    private RecyclerView.LayoutManager mLayoutManager;
 
-    private final static String API_KEY = BuildConfig.API_KEY;
+    private List<MeteorObjects> list = new ArrayList<MeteorObjects>();
+
+    public final static String API_KEY = BuildConfig.API_KEY;
     private static final String TAG = MainActivity.class.getSimpleName();
-    private Observable<NEO> observable;
+
+    private LiveDataNeoViewModel mLiveDataNeoViewModel;
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -45,14 +50,9 @@ public class MainActivity extends AppCompatActivity {
         @Override
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
             switch (item.getItemId()) {
-                case R.id.navigation_home:
-                    mTextMessage.setText(R.string.title_home);
+                case R.id.navigation_today:
                     return true;
-                case R.id.navigation_dashboard:
-                    mTextMessage.setText(R.string.title_dashboard);
-                    return true;
-                case R.id.navigation_notifications:
-                    mTextMessage.setText(R.string.title_notifications);
+                case R.id.navigation_explore:
                     return true;
             }
             return false;
@@ -65,43 +65,72 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mTextMessage = (TextView) findViewById(R.id.message);
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
 
         if (API_KEY.isEmpty()) {
-            Toast.makeText(getApplicationContext(), "Please obtain your API KEY", Toast.LENGTH_LONG).show();
+            Toast.makeText(getApplicationContext(), "Please obtain your API KEY, Place in root folder in file keystore.properties", Toast.LENGTH_LONG).show();
             return;
         }
-        NEOService neoService = ApiClient.getClient().create(NEOService.class);
-        observable = neoService.getFeeds("2015-09-07","2015-09-08",API_KEY);
-        observable
-                .subscribeOn(Schedulers.io())
-                .observeOn(AndroidSchedulers.mainThread())
-                .subscribe(new Observer<NEO>() {
-                    @Override
-                    public void onSubscribe(@io.reactivex.annotations.NonNull Disposable d) {
 
-                    }
 
-                    @Override
-                    public void onNext(@io.reactivex.annotations.NonNull NEO neo) {
-                        Log.d(TAG, "Count of Near Earth Objects " + neo.getNearEarthObjects().get(0).getDate());
-                    }
-
-                    @Override
-                    public void onError(@io.reactivex.annotations.NonNull Throwable e) {
-
-                    }
-
-                    @Override
-                    public void onComplete() {
-
-                    }
-                });
-//                .subscribe( response -> {
-//                    Log.d(TAG, "Count of Near Earth Objects " + response.getNearEarthObjects().get(0).getDate());
+        mRecyclerView = (RecyclerView) findViewById(R.id.recycler_view);
+        mRecyclerView.setHasFixedSize(true);
+        mLayoutManager = new LinearLayoutManager(this);
+        mRecyclerView.setLayoutManager(mLayoutManager);
+        mAdapter = new NEOAdapter(getApplicationContext(), list);
+        mRecyclerView.setAdapter(mAdapter);
+//
+//        NEOService neoService = ApiClient.getClient().create(NEOService.class);
+//        observable = neoService.getFeeds("2015-09-07","2015-09-07",API_KEY);
+//        observable
+//                .subscribeOn(Schedulers.io())
+//                .observeOn(AndroidSchedulers.mainThread())
+//                .subscribe(new Observer<NEO>() {
+//                    @Override
+//                    public void onSubscribe(@io.reactivex.annotations.NonNull Disposable d) {
+//
+//                    }
+//
+//                    @Override
+//                    public void onNext(@io.reactivex.annotations.NonNull NEO neo) {
+//                        List<MeteorObjects> result = neo.getNearEarthObjects().get(0).getMeteorObjects();
+//                        for(MeteorObjects mo : result){
+//                            list.add(mo);
+//                        }
+//                        Log.d(TAG, "Count of Near Earth Objects " + list.size());
+//                        mAdapter.notifyDataSetChanged();
+//                    }
+//
+//                    @Override
+//                    public void onError(@io.reactivex.annotations.NonNull Throwable e) {
+//
+//                    }
+//
+//                    @Override
+//                    public void onComplete() {
+//
+//                    }
 //                });
+////                .subscribe( response -> {
+////                    Log.d(TAG, "Count of Near Earth Objects " + response.getNearEarthObjects().get(0).getDate());
+////                });
+
+        mLiveDataNeoViewModel = ViewModelProviders.of(this).get(LiveDataNeoViewModel.class);
+        subscribe();
+    }
+
+    private void subscribe() {
+        final Observer<NEO> neoObserver = new Observer<NEO>() {
+            @Override
+            public void onChanged(@Nullable NEO neo) {
+                List<MeteorObjects> results = neo.getNearEarthObjects().get(0).getMeteorObjects();
+                list.addAll(results);
+                Log.d(TAG, "Count of Near Earth Objects " + neo.getElementCount());
+                mAdapter.notifyDataSetChanged();
+            }
+        };
+        mLiveDataNeoViewModel.getNeoObservable().observe(this, neoObserver);
     }
 
     @Override
